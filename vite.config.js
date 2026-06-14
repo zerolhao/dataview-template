760eee7428aff35065a32bf48b021fff5f2d6102
zerolhao <zerolhao@outlook.com>
2026-06-14 13:35:25 +0800

    chore: 添加自适应

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
+import postcssPxtorem from 'postcss-pxtorem' // 引入转换插件
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -14,6 +15,17 @@ export default defineConfig({
     outDir: 'dist',
   },
   plugins: [vue(), vueJsx(), vueDevTools(), tailwindcss()],
+  css: {
+    postcss: {
+      plugins: [
+        postcssPxtorem({
+          // 使用 PostCSS 插件进行 px 转换
+          rootValue: 16, // 根据设计稿调整，例如：375设计稿用37.5，750设计稿用75
+          propList: ['*'],
+        }),
+      ],
+    },
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
